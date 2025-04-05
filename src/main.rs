566c2a047038ba6a72f3a4cd5d0d997128736421
dograkunal <kunaldogra31@gmail.com>
2025-04-05 20:13:16 +0530

    minor changes

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,15 @@ mod tasks;
 
 use std::io;
 use std::io::Write;
-use tasks::{add_task, view_tasks, mark_task_as_completed, remove_task, Task};
+use tasks::{add_task, mark_task_as_completed, remove_task, view_tasks, Task};
 
-
-
-fn main(){
-
+fn main() {
     //create a vector of tasks
     //Vector is a collection of items in a specific order
     //Vec is short for vector
     let mut tasks: Vec<Task> = Vec::new();
 
-
-    loop{
+    loop {
         println!("\n");
         println!("--------------------------------");
         println!("To-Do List");
@@ -24,23 +20,23 @@ fn main(){
         println!("3. Mark task as completed");
         println!("4. Remove task");
         println!("5. Exit");
-
+        println!("--------------------------------");
 
         print!("Choose an option: ");
         //force print without newline
         //flush is a method that flushes the buffer to the console
         //unwrap is a method that returns the value of the buffer
-        io::stdout().flush().unwrap(); 
-
+        io::stdout().flush().unwrap();
 
         //get user input
         let mut choice = String::new();
         //stdIn is a module that provides the stdin function
         //read_line is a method that reads a line from the console and stores it in a string
-        io::stdin().read_line(&mut choice).expect("Failed to read line");
+        io::stdin()
+            .read_line(&mut choice)
+            .expect("Failed to read line");
 
-
-        match choice.trim().parse(){
+        match choice.trim().parse() {
             Ok(1) => add_task(&mut tasks),
             Ok(2) => view_tasks(&tasks),
             Ok(3) => mark_task_as_completed(&mut tasks),
--- src/tasks.rs
+++ src/tasks.rs
@@ -3,74 +3,63 @@ use std::io;
 // #[derive(Debug)] lets us print the task with println!("{:?}", task)
 #[derive(Debug)]
 
-
 //struct is a keyword that defines a new data type
 pub struct Task {
     //description: what the task is
     pub description: String,
 
     // completed: whether it's done or not (true/false)
-   pub completed: bool,
-   pub id: u32,
+    pub completed: bool,
+    pub id: u32,
 }
 
-
-
 //------------------------------------------------------------//
-
 
 //pub is a keyword that makes the function public
-pub fn add_task(tasks: &mut Vec<Task>){
-    
-let mut description = String::new();
-println!("Enter the description of the task: ");
+pub fn add_task(tasks: &mut Vec<Task>) {
+    let mut description = String::new();
+    println!("Enter the description of the task: ");
 
-//read_line is a method that reads a line from the console and stores it in a string
-io::stdin().read_line(&mut description).expect("Failed to read line");
+    //read_line is a method that reads a line from the console and stores it in a string
+    io::stdin()
+        .read_line(&mut description)
+        .expect("Failed to read line");
 
-let task = Task {
-description: description.trim().to_string(),
-completed: false,
-id: tasks.len() as u32 + 1,
-};
+    let task = Task {
+        description: description.trim().to_string(),
+        completed: false,
+        id: tasks.len() as u32 + 1,
+    };
 
-//push is a method that adds an element to the end of the vector
-tasks.push(task);
-println!("Task added successfully!");
+    //push is a method that adds an element to the end of the vector
+    tasks.push(task);
+    println!("Task added successfully!");
 }
-
-
-
 
 //------------------------------------------------------------//
 
+pub fn view_tasks(tasks: &Vec<Task>) {
+    if tasks.is_empty() {
+        println!("No tasks found");
+        return;
+    }
 
-
-pub fn view_tasks(tasks: &Vec<Task>){
-if tasks.is_empty(){
-    println!("No tasks found");
-    return;
+    //iter is a method that returns an iterator over the elements of the vector
+    //enumerate is a method that returns a tuple of the index and the element
+    for (i, task) in tasks.iter().enumerate() {
+        let status = if task.completed {
+            "Completed"
+        } else {
+            "Not Completed"
+        };
+        println!("{}: {} - {}", i + 1, task.description, status);
+    }
 }
-
-//iter is a method that returns an iterator over the elements of the vector
-//enumerate is a method that returns a tuple of the index and the element
-for(i, task) in tasks.iter().enumerate(){
-    let status = if task.completed { "Completed" } else { "Not Completed" };
-    println!("{}: {} - {}", i + 1, task.description, status);
-}
-
-
-
-}
-
-
 
 //------------------------------------------------------------//
 
-
-
-pub fn mark_task_as_completed(tasks: &mut Vec<Task>){
-view_tasks(tasks);
+pub fn mark_task_as_completed(tasks: &mut Vec<Task>) {
+    view_tasks(tasks);
 
     println!("Enter the id of the task you want to mark as completed: ");
     let mut id = String::new();
@@ -82,21 +71,17 @@ view_tasks(tasks);
 
     //get_mut is a method that returns a mutable reference to the element at the given index
     // Some is a keyword that returns an optional value
-    if let Some(task) = tasks.get_mut(id as usize - 1){
+    if let Some(task) = tasks.get_mut(id as usize - 1) {
         task.completed = true;
         println!("Task marked as completed!");
     } else {
         println!("Invalid task id");
-    }   
-
+    }
 }
-
-
 
 //------------------------------------------------------------//
 
-
-pub fn remove_task(tasks: &mut Vec<Task>){
+pub fn remove_task(tasks: &mut Vec<Task>) {
     view_tasks(tasks);
 
     println!("Enter the id of the task you want to remove: ");
@@ -111,7 +96,7 @@ pub fn remove_task(tasks: &mut Vec<Task>){
         Ok(id) if id > 0 && (id as usize) <= tasks.len() => {
             tasks.remove((id - 1) as usize);
             println!("Task deleted!");
-        },
+        }
         _ => {
             println!("Please enter a valid task id.");
         }
