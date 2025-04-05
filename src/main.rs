141e3dd4865053e41a51da940e91228435944c01
dograkunal <kunaldogra31@gmail.com>
2025-04-05 20:10:31 +0530

    cli calculator in rust

---
+++ src/main.rs
@@ -0,0 +1,54 @@
+mod tasks;
+
+use std::io;
+use std::io::Write;
+use tasks::{add_task, view_tasks, mark_task_as_completed, remove_task, Task};
+
+
+
+fn main(){
+
+    //create a vector of tasks
+    //Vector is a collection of items in a specific order
+    //Vec is short for vector
+    let mut tasks: Vec<Task> = Vec::new();
+
+
+    loop{
+        println!("\n");
+        println!("--------------------------------");
+        println!("To-Do List");
+        println!("--------------------------------");
+        println!("1. Add a task");
+        println!("2. View tasks");
+        println!("3. Mark task as completed");
+        println!("4. Remove task");
+        println!("5. Exit");
+
+
+        print!("Choose an option: ");
+        //force print without newline
+        //flush is a method that flushes the buffer to the console
+        //unwrap is a method that returns the value of the buffer
+        io::stdout().flush().unwrap(); 
+
+
+        //get user input
+        let mut choice = String::new();
+        //stdIn is a module that provides the stdin function
+        //read_line is a method that reads a line from the console and stores it in a string
+        io::stdin().read_line(&mut choice).expect("Failed to read line");
+
+
+        match choice.trim().parse(){
+            Ok(1) => add_task(&mut tasks),
+            Ok(2) => view_tasks(&tasks),
+            Ok(3) => mark_task_as_completed(&mut tasks),
+            Ok(4) => remove_task(&mut tasks),
+            Ok(5) => break,
+            //if the user enters an invalid option, print an error message
+            //_ is a wildcard that matches any value
+            _ => println!("Invalid option"),
+        }
+    }
+}
